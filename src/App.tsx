1cddf08e7069cbb37d51e93299d1dc2c37156d34
RajilSaj <svembe@gmail.com>
2023-03-08 03:06:14 -0500

    Tailwind Set

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './App.css';
+// eslint-disable-next-line 
+import imageTest from './assets/images/cami_1.jpg';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World !</h1>
+    <div className=" App mx-auto py-8 container ">
+      <img src={imageTest} className='border-4 border-cami-yellow-light rounded-full mx-auto object-cover w-96 h-96' alt='imageTest'/>
+      <h1 className=' text-8xl text-center mt-8 text-cami-red'>Site web en construction.</h1>
+      <footer className='mt-10 pt-2 text-center text-gray-700'>
+          &copy;<span>2023, RAJILSAJ</span>
+      </footer>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,18 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme : {
+    extend : {
+      fontFamily : {
+        custom : ["Ardela", "sans-serif"]
+      },
+      colors : {
+        'cami-red': '#D93240',
+        'cami-red-black': '#D93240',
+        'cami-yellow-light': '#F2E422',
+        'cami-yellow-black': '#A69F3C', 
+      }
+    }
+  },
   plugins: [],
 }
